5231a525bb5bbf3d8e30041c22ea742c74e1a131
torbenconto <torbenmconto@gmail.com>
2024-02-27 02:12:40 -0500

    Simple versions enum for now

---
+++ src/bible/versions.ts
@@ -0,0 +1,19 @@
+enum Versions {
+    AMP= "AMP",
+    NIV = "NIV",
+    ESV = "ESV",
+    KJV = "KJV",
+    NKJv = "NKJV",
+    ASV = "ASV",
+    BSB = "BSB",
+    NASB = "NASB",
+    DBT = "DBT",
+    DRB = "DRB",
+    ERV = "ERV",
+    YLT = "YLT",
+    NLT = "NLT",
+    EASY = "EASY",
+    CSB = "CSB",
+}
+
+export default Versions;
