86c878f7cfdbbc93f63ac3212726bd12258aac2e
torbenconto <torbenmconto@gmail.com>
2024-02-27 21:48:41 -0500

    .env

--- src/bible/api.ts
+++ src/bible/api.ts
@@ -2,26 +2,28 @@ import {Book} from "./books.ts";
 import {Version} from "./versions.ts";
 import axios from "axios";
 
+const api_url = process.env.API_URL;
+
 export async function GetBook(version: Version, book: Book): Promise<string> {
     // Http request to get chapter
-    const chapterReq = await axios.get(`http://localhost:8080/book/${book}?version=${version}`)
+    const chapterReq = await axios.get(`${api_url}/book/${book}?version=${version}`)
     return chapterReq.data;
 }
 
 export async function GetBooks(version: Version): Promise<string[]> {
     // Http request to get books
-    const booksReq = await axios.get(`http://localhost:8080/books?version=${version}`)
+    const booksReq = await axios.get(`${api_url}/books?version=${version}`)
     return booksReq.data;
 }
 
 
 export async function GetChapterCount(version: Version, book: Book): Promise<number> {
     // Http request to get chapter count
-    const chapterCountReq = await axios.get(`http://localhost:8080/chaptercount/${book}?version=${version}`)
+    const chapterCountReq = await axios.get(`${api_url}/chaptercount/${book}?version=${version}`)
     return chapterCountReq.data;
 }
 
 export async function Explain(verse: string, version: Version): Promise<string> {
-    const explainReq = await axios.get(`http://localhost:8080/explain/${verse}?version=${version}`)
+    const explainReq = await axios.get(`${api_url}/explain/${verse}?version=${version}`)
     return explainReq.data;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import dotenv from 'dotenv'
 import App from './App.tsx'
 import './index.css'
+
+(() => {
+    dotenv.config();
+})()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
