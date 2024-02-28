c268fe93a2b90b5ffe31051e53ecfacf3af2808d
torbenconto <torbenmconto@gmail.com>
2024-02-27 23:11:43 -0500

    Remove extra close button

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,11 +5,6 @@ function Modal({ onClose, children }: { onClose: () => void; children: React.Rea
         <div
             className="fixed bottom-0 left-1/2 transform -translate-x-1/2 md:w-1/3 w-full px-4 shadow-xl rounded-t-xl bg-white border border-black">
             <div className="mx-auto md:p-4">
-                <div className="flex justify-between items-center border-b border-gray-300">
-                    <button className="text-gray-500 hover:text-gray-700 ml-auto text-lg" onClick={onClose}>
-                        Close
-                    </button>
-                </div>
                 <button className="text-gray-500 hover:text-gray-700 ml-auto text-lg" onClick={onClose}>
                     Close
                 </button>
