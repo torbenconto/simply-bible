6fccae327423cfdc2b1c35fa0537b3680074c0d6
torbenconto <torbenmconto@gmail.com>
2024-02-27 22:07:31 -0500

    Allow origins

--- api/main.go
+++ api/main.go
@@ -37,7 +37,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://127.0.0.1:5173", "http://localhost:5173"},
+		AllowOrigins:     []string{"http://127.0.0.1:5173", "http://localhost:5173", "https://simply-bible.vercel.app/"},
 		AllowMethods:     []string{"GET"},
 		AllowHeaders:     []string{"Origin"},
 		ExposeHeaders:    []string{"Content-Length"},
